b4411ab242ae4e7e02cf3ea1f92305dc2080f9e1
Lee Zhen Lek <59497285+ZhEnLEK@users.noreply.github.com>
2024-02-28 18:35:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,16 +15,11 @@ def main():
     st.title('After Sales Churn Predictor')
     st.sidebar.header('Data Input')
 
-    # Collect user input features
+   
     rate_of_service = st.sidebar.slider('Rate of service', 0.0, 6.0, 3.0, step=0.1)
-    
-    # Apply scaling to numerical features
+  
     scaled_rate_of_service = scaler.transform([[rate_of_service]])[0][0]
  
-    # Apply scaling to more numerical features...
-
-    # Categorical features
-
 
     # Define dropdown lists for each one-hot encoded categorical feature
     model = st.sidebar.selectbox('Vehicle model', ['Model_general_ACCORD', 'Model_general_BR-V', 'Model_general_CITY',
@@ -37,9 +32,7 @@ def main():
        'Vehicle_age_13', 'Vehicle_age_14', 'Vehicle_age_15', 'Vehicle_age_2',
        'Vehicle_age_3', 'Vehicle_age_4', 'Vehicle_age_5', 'Vehicle_age_6',
        'Vehicle_age_7', 'Vehicle_age_8', 'Vehicle_age_9', 'Vehicle_age_>15'])
-
-
-    # Map dropdown list values to numerical values
+  
     model_mapping = {'Model_general_ACCORD': 0, 'Model_general_BR-V': 1, 'Model_general_CITY': 2,
        'Model_general_CIVIC': 3, 'Model_general_CR-V': 4, 'Model_general_CR-Z': 5,
        'Model_general_FREED': 6, 'Model_general_HR-V': 7, 'Model_general_INSIGHT': 8,
@@ -58,7 +51,7 @@ def main():
 
     # Make predictions
     input_data = {
-     
+       
         'rate_of_service_normalized': scaled_rate_of_service,
         'Model_general_ACCORD': 1 if model_encoded == 0 else 0,
         'Model_general_BR-V': 1 if model_encoded == 1 else 0,
@@ -93,9 +86,9 @@ def main():
          'Vehicle_age_8': 1 if age_encoded == 14 else 0,
          'Vehicle_age_8': 1 if age_encoded == 14 else 0,
          'Vehicle_age_9': 1 if age_encoded == 15 else 0,
-         'Vehicle_age_>15': 1 if age_encoded == 16 else 0,
-       
-      
+         'Vehicle_age_>15': 1 if age_encoded == 16 else 0
+        
+   
     }
     input_df = pd.DataFrame([input_data])
     prediction = model.predict(input_df)
