319d1c526fe78fe6ab32749014353bbd3dfa29a1
Lee Zhen Lek <59497285+ZhEnLEK@users.noreply.github.com>
2024-02-29 16:53:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,17 +16,25 @@ def preprocess_input(model_general, vehicle_age, rate_of_service):
     model_general_encoded = pd.get_dummies(input_data['model_general'], prefix='Model_general')
     # Ensure all possible columns are present, even if the current selection doesn't cover all of them
     all_model_general_columns = ['Model_general_ACCORD', 'Model_general_BR-V', 'Model_general_CITY', 'Model_general_CIVIC', 'Model_general_CR-V', 'Model_general_CR-Z', 'Model_general_FREED', 'Model_general_HR-V', 'Model_general_INSIGHT', 'Model_general_JAZZ', 'Model_general_ODYSSEY', 'Model_general_OTHERS', 'Model_general_PRELUDE', 'Model_general_STREAM']
+    
+  
     for column in all_model_general_columns:
-        if column not in model_general_encoded.columns:
-            model_general_encoded[column] = 0
+    if column in model_general_encoded.columns:
+        model_general_encoded[column] = 1
+    else:
+        model_general_encoded[column] = 0
     
     # One-hot encode 'vehicle_age'
     vehicle_age_encoded = pd.get_dummies(input_data['vehicle_age'], prefix='Vehicle_age')
     # Ensure all possible columns are present, even if the current selection doesn't cover all of them
     all_vehicle_age_columns = ['Vehicle_age_0', 'Vehicle_age_1', 'Vehicle_age_10', 'Vehicle_age_11', 'Vehicle_age_12', 'Vehicle_age_13', 'Vehicle_age_14', 'Vehicle_age_15', 'Vehicle_age_2', 'Vehicle_age_3', 'Vehicle_age_4', 'Vehicle_age_5', 'Vehicle_age_6', 'Vehicle_age_7', 'Vehicle_age_8', 'Vehicle_age_9', 'Vehicle_age_>15']
+   
     for column in all_vehicle_age_columns:
-        if column not in vehicle_age_encoded.columns:
-            vehicle_age_encoded[column] = 0
+    if column in vehicle_age_encoded.columns:
+        vehicle_age_encoded[column] = 1
+    else:
+        vehicle_age_encoded[column] = 0
+
     
     scaler = MinMaxScaler()
     rate_of_service_normalized = scaler.fit_transform([[rate_of_service]])[0][0]
